b3c85997b40c294745a5986a576128ebe73d0bd3
Annie Weng <aweng@hotmail.com>
2021-09-20 11:23:16 -0400

    Create monitor_nginx_config.sh

---
+++ monitor_nginx_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###########
+#monitor nginx folder, and reload nginx if detecth create/modify/delete event
+while true
+do
+ inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx
+ nginx -t
+ if [ $? -eq 0 ]
+ then
+  echo "Detected Nginx Configuration Change"
+  echo "Executing: nginx -s reload"
+  nginx -s reload
+ fi
+done
